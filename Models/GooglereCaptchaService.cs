fb3fffb5b521218dcdb5f0f73668d0054a873caa
Adriano Damas Batistaano <adriano.damas@gmail.com>
2021-04-01 09:42:04 -0300

    Criação de uma POC que contempla a utilização do Google ReCaptcha no modo invisível.

---
+++ Models/GooglereCaptchaService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace GoogleRecap.Models
+{
+    public class GooglereCaptchaService
+    {
+        private ReCAPTCHASettings _settings;
+
+        public GooglereCaptchaService(IOptions<ReCAPTCHASettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public virtual async Task<GoogleREspo> VerifyreCaptcha(string _Token)
+        {
+            GooglereCaptchaData _MyData = new GooglereCaptchaData
+            {
+                response=_Token,
+                secret = _settings.ReCAPTCHA_Secret_Key
+            };
+
+            HttpClient client = new HttpClient();
+
+            var repponse = await client.GetStringAsync($"https://www.google.com/recaptcha/api/siteverify?=secret{_MyData.secret}&response={_MyData.response}");
+
+            var capresp = JsonConvert.DeserializeObject<GoogleREspo>(repponse);
+
+            return capresp;
+
+        }
+    }
+
+    public class GooglereCaptchaData
+    {
+        public string response { get; set; } //token
+
+        public string secret { get; set; }
+    }
+   
+    public class GoogleREspo
+    {
+        public bool success { get; set; }
+        public double score { get; set; }
+        public string action { get; set;}
+        public DateTime challenge_ts { get; set; }
+        public string hostname { get; set; }
+    }
+}
---
+++ Models/httpClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GoogleRecap.Models
+{
+    internal class httpClient
+    {
+        internal Task GetStringAsync(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
